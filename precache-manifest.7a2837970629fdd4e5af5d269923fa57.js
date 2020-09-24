005a16a86f92492bafc30962ab758477681c31bd
shawnjonchan <xiaojiechen86@gmail.com>
2020-09-24 09:49:30 +0800

    deploy

---
+++ precache-manifest.7a2837970629fdd4e5af5d269923fa57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f946828657209121eaff",
+    "url": "/morney-banana-website/css/app.7cf48fb3.css"
+  },
+  {
+    "revision": "ee9e5ebf31da995ee56ab6354449ac81",
+    "url": "/morney-banana-website/index.html"
+  },
+  {
+    "revision": "f946828657209121eaff",
+    "url": "/morney-banana-website/js/app.ddd2590e.js"
+  },
+  {
+    "revision": "eb0652816475d4567282",
+    "url": "/morney-banana-website/js/chunk-vendors.d5a83c01.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-banana-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-banana-website/robots.txt"
+  }
+]);
